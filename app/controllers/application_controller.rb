7d44608145897a1ede68ba4dabcebe9541574b55
kyontanyk <knaka@song.ocn.ne.jp>
2024-04-17 21:57:56 +0900

    Finish p109(Chapter6-4).

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
   layout :set_layout
 
+  class Forbidden < ActionController::ActionControllerError
+  end
+  class IpAddressRejected < ActionController::ActionControllerError
+  end
+
   rescue_from StandardError, with: :rescue500
+  rescue_from Forbidden, with: :rescue403
+  rescue_from IpAddressRejected, with: :rescue403
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue404
 
   private
 
@@ -11,6 +19,15 @@ class ApplicationController < ActionController::Base
     else
       "customer"
     end
+  end
+
+  def rescue403(e)
+    @exception = e
+    render 'errors/forbidden', status: 403
+  end
+
+  def rescue404(e)
+    render 'errors/not_found', status: 404
   end
 
   def rescue500(e)
