8000b4544906222a6f5567214f09ef6ba8394347
kyontanyk <knaka@song.ocn.ne.jp>
2024-06-23 21:12:34 +0900

    Finish p271(Chapter13-1).

--- app/controllers/staff/sessions_controller.rb
+++ app/controllers/staff/sessions_controller.rb
@@ -1,5 +1,7 @@
 class Staff::SessionsController < Staff::Base
 
+  require './app/services/shared/constant_values'
+
   skip_before_action :authorize
 
   def new
@@ -17,12 +19,14 @@ class Staff::SessionsController < Staff::Base
         'LOWER(email) = ?',
         @form.email.downcase)
     end
-    if !staff_member.nil? \
+    if staff_member.present? \
       && Staff::Authenticator.new(staff_member).authenticate(@form.password)
       if staff_member.suspended?
+        staff_member.events.create!(type: EventType::REJECTED)
         flash.now.alert = 'アカウントが停止されています。'
         render action: 'new'
       else
+        staff_member.events.create!(type: EventType::LOGIN)
         session[:staff_member_id] = staff_member.id
         session[:last_access_time] = Time.current
         flash.notice = 'ログインしました。'
@@ -35,6 +39,9 @@ class Staff::SessionsController < Staff::Base
   end
 
   def destroy
+    if current_staff_member
+      current_staff_member.events.create!(type: EventType::LOGOUT)
+    end
     session.delete(:staff_member_id)
     flash.notice = 'ログアウトしました。'
     redirect_to :staff_root
---
+++ app/services/shared/constant_values.rb
@@ -0,0 +1,5 @@
+module EventType
+  LOGIN = 'logged_in'
+  REJECTED = 'rejected'
+  LOGOUT = 'logged_out'
+end
