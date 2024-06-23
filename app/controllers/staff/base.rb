4acd24916d0c2c94b9f5861cd5a1ca5d8149a9ab
kyontanyk <knaka@song.ocn.ne.jp>
2024-06-23 10:10:32 +0900

    Finish p248(Chapter12-1).

--- app/controllers/staff/base.rb
+++ app/controllers/staff/base.rb
@@ -1,7 +1,15 @@
 class Staff::Base < ApplicationController
-  private
 
-  def current_staff_member
+  before_action :authorize
+
+  private def authorize
+    unless current_staff_member
+      flash.alert = '職員としてログインしてください。'
+      redirect_to :staff_login
+    end
+  end
+
+  private def current_staff_member
     if session[:staff_member_id]
       @current_staff_member \
         ||= StaffMember.find_by(id: session[:staff_member_id])
