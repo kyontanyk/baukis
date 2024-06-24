4fabafa0470e5deeace5fe4cf4e77dea3265edbc
kyontanyk <knaka@song.ocn.ne.jp>
2024-06-24 15:31:43 +0900

    Finish p301(Chapter14-1-3).

---
+++ app/models/concerns/string_normalizer.rb
@@ -0,0 +1,13 @@
+require 'nkf'
+
+module StringNormalizer
+  extend ActiveSupport::Concern
+
+  def normalize_as_name(text)
+    NKF.nkf('-WwZ1', text).strip if text
+  end
+
+  def normalize_as_furigana(text)
+    NKF.nkf('-WwZ1 --katakana', text).strip if text
+  end
+end
