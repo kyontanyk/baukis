f1d84c9e5545c2c93e1422c89f45e658c6f9aa16
kyontanyk <knaka@song.ocn.ne.jp>
2024-06-28 11:59:08 +0900

    Finish p384(Chapter16-3).

---
+++ app/presenters/user_form_presenter.rb
@@ -0,0 +1,20 @@
+class UserFormPresenter < FormPresenter
+  def password_field_block(name, label_text, options = {})
+    # 新規ユーザー登録時のみパスワード入力欄を表示し、
+    # 既存ユーザー編集画面ではパスワード入力欄を表示しない。
+    if form_builder.respond_to?('new_record?') &&
+      form_builder.new_record?
+      super(name, label_text, options)
+    end
+  end
+
+  def full_name_block(name1, name2, label_text, options = {})
+    markup(:div, class: 'input_block') do |m|
+      m << decorated_label(name1, label_text, options)
+      m << text_field(name1, options)
+      m << text_field(name2, options)
+      m << error_messages_for(name1)
+      m << error_messages_for(name2)
+    end
+  end
+end
