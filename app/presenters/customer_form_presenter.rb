3c9defacde08a7f8a03997ae95e1866b9dc1630e
kyontanyk <knaka@song.ocn.ne.jp>
2024-06-30 17:25:59 +0900

    Finish p404(Chapter17-2-1).

--- app/presenters/customer_form_presenter.rb
+++ app/presenters/customer_form_presenter.rb
@@ -2,7 +2,7 @@ class CustomerFormPresenter < UserFormPresenter
   def gender_field_block
     markup(:div, class: 'radio-buttons') do |m|
       m << decorated_label(:gender, '性別')
-      m << radio_button(:gender, 'mail')
+      m << radio_button(:gender, 'male')
       m << label(:gender_male, '男性')
       m << radio_button(:gender, 'female')
       m << label(:gender_female, '女性')
