e5732931e9a2dd27d654135bc657c489d619b64c
kyontanyk <knaka@song.ocn.ne.jp>
2024-07-02 11:50:18 +0900

    Finish p451(Chapter18-3-2).

--- app/forms/staff/customer_form.rb
+++ app/forms/staff/customer_form.rb
@@ -24,6 +24,16 @@ class Staff::CustomerForm
 
     customer.assign_attributes(customer_params)
 
+    phones = phone_params(:customer).fetch(:phones)
+    customer.personal_phones.size.times do |index|
+      attributes = phones[index.to_s]
+      if attributes && attributes[:number].present?
+        customer.personal_phones[index].assign_attributes(attributes)
+      else
+        customer.personal_phones[index].mark_for_destruction
+      end
+    end
+
     if inputs_home_address
       customer.home_address.assign_attributes(home_address_params)
     else
@@ -41,20 +51,26 @@ class Staff::CustomerForm
   end
 
   private def customer_params
-    @params.require(:customer).permit(
+    @params.require(:customer).except(:phones).permit(
       :email, :password,
       :family_name, :given_name, :family_name_kana, :given_name_kana,
       :birthday, :gender)
   end
 
   private def home_address_params
-    @params.require(:home_address).permit(
+    @params.require(:home_address).except(:phones).permit(
       :postal_code, :prefecture, :city, :address1, :address2)
   end
 
   private def work_address_params
-    @params.require(:work_address).permit(
+    @params.require(:work_address).except(:phones).permit(
       :postal_code, :prefecture, :city, :address1, :address2,
       :company_name, :division_name)
+  end
+
+  private def phone_params(record_name)
+    @params.require(record_name).slice(:phones).permit(
+      phones: [ :number, :primary ]
+    )
   end
 end
